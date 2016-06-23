cf9ae5460e8668760c6c583f2e74dc586e4fd8a8
Robert Quattlebaum <darco@deepdarc.com>
2016-06-23 11:07:34 -0700

    minor fix

--- upload-to-device.sh
+++ upload-to-device.sh
@@ -5,6 +5,6 @@
 	exit 1
 }
 
-gtar -c -C root --owner=0 --group=0 . | ssh $1 "rm bin/smcpd ; tar -x -v -f - -C / ; save ; ./rc.poststart"
+gtar -c -C root --owner=0 --group=0 . | ssh $1 "rm bin/smcpd ; tar -x -v -f - -C / ; cfgmtd -w -p /etc/ ; ./rc.poststart"
 #gtar cC root | ssh $1 "tar xvC / ; reboot"
 
