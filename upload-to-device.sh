c67ae1239ff4a259bc7bbdeeb492fa87bb938d26
Robert Quattlebaum <darco@deepdarc.com>
2016-06-28 00:43:58 -0700

    more updates

--- upload-to-device.sh
+++ upload-to-device.sh
@@ -5,6 +5,7 @@
 	exit 1
 }
 
-gtar -c -C root --owner=0 --group=0 . | ssh $1 "rm bin/smcpd ; tar -x -v -f - -C / ; cfgmtd -w -p /etc/ ; ./rc.poststart"
+#gtar -c -C root --owner=0 --group=0 . | ssh $1 "rm bin/smcpd ; tar -x -v -f - -C / ; cfgmtd -w -p /etc/ ; ./rc.poststart"
+gtar -c -C root --owner=0 --group=0 . | ssh $1 "rm bin/smcpd ; tar -x -v -f - -C / ; killall smcpd ; ./rc.poststart"
 #gtar cC root | ssh $1 "tar xvC / ; reboot"
 
