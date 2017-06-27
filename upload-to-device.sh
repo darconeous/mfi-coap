adc540bf9c097363c8ca98b5033d3223eba7152b
Robert Quattlebaum <darco@deepdarc.com>
2017-06-26 20:06:18 -0700

    minor updates

--- upload-to-device.sh
+++ upload-to-device.sh
@@ -6,6 +6,6 @@
 }
 
 #gtar -c -C root --owner=0 --group=0 . | ssh $1 "rm bin/smcpd ; tar -x -v -f - -C / ; cfgmtd -w -p /etc/ ; ./rc.poststart"
-gtar -c -C root --owner=0 --group=0 . | ssh $1 'cd / ; rm $(cat /var/etc/persistent/mfi-coap-manifest.txt) ; tar -x -v -f - -C / ; killall smcpd ; cd /var/etc/persistent ; ./rc.poststart'
+gtar -c -C root --owner=0 --group=0 . | ssh -oKexAlgorithms=+diffie-hellman-group1-sha1 $1 'cd / ; rm $(cat /var/etc/persistent/mfi-coap-manifest.txt) ; tar -x -v -f - -C / ; killall smcpd ; cd /var/etc/persistent ; ./rc.poststart'
 #gtar cC root | ssh $1 "tar xvC / ; reboot"
 
